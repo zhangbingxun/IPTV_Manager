a96e4df5c00418b7d56e3fcaa7b57fa1a8cbdbda
kwangaho <kwangaho@gmail.com>
2020-03-03 07:23:11 +0800

    Update

--- apps/secretkeyController.php
+++ apps/secretkeyController.php
@@ -8,17 +8,17 @@ if (isset($_POST['secret_key_enter'])) {
     $get_key = get_config('secret_key');
     if (empty($get_key)) {
         $_SESSION['secret_key_status'] = '1';
-        echo '<script language=JavaScript>lightyear.loading("hide");location.replace(location.href);</script>';
+        echo '<script language=JavaScript>location.replace(location.href);</script>';
     } else {
         if ($secret_key == $get_key) {
             $_SESSION['secret_key_status'] = '1';
             if (isset($_POST['remembersecret_key'])) {
-                setcookie("secret_key", $row['value'], time() + 3600 * 24 * 7);
+                setcookie("secret_key", $get_key, time() + 3600 * 24 * 7);
                 setcookie("remembersecret_key", "1", time() + 3600 * 24 * 7);
             } else {
                 setcookie("remembersecret_key", "1", time()-3600);
             } 
-            echo '<script language=JavaScript>lightyear.loading("hide");location.replace(location.href);</script>';
+            echo '<script language=JavaScript>location.replace(location.href);</script>';
         } else {
             echo "<script>alert('安全码错误！');</script>";
         } 
@@ -30,6 +30,7 @@ if (isset($_COOKIE['remembersecret_key'])) {
     $get_key = get_config('secret_key');
     if ($secret_key == $get_key) {
         $_SESSION['secret_key_status'] = '1';
+            echo '<script language=JavaScript>location.replace(location.href);</script>';
     } 
 } 
 

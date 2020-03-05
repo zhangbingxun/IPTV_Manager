645d7900a3bf29d1d435232b24f13afad2da0998
luo2888 <kwangaho@gmail.com>
2020-03-05 22:20:37 +0800

    Update

--- views/sysadmin/password.view.php
+++ views/sysadmin/password.view.php
@@ -1,3 +1,4 @@
+<?php $secret_key=get_config('secret_key'); ?>
 <div class="row">
 	<div class="col-lg-12">
 		<div class="card">
@@ -14,7 +15,7 @@
 							</div>
 							<div class="form-group">
 								<button class="btn btn-label btn-primary" type="submit" name="submit" value=""><label><i class="mdi mdi-checkbox-marked-circle-outline"></i></label>修改安全码</button>
-								<button class="btn btn-danger" type="submit" name="closesecret_key" value="" <?php if(empty(get_config('secret_key'))){echo 'disabled';} ?>><?php if (empty(get_config('secret_key'))){echo '安全码验证已关闭';} else {echo '关闭安全码验证';} ?></button>
+								<button class="btn btn-danger" type="submit" name="closesecret_key" value="" <?php if(empty($secret_key)){echo 'disabled';} ?>><?php if (empty($secret_key)){echo '安全码验证已关闭';} else {echo '关闭安全码验证';} ?></button>
 							</div>
 						</form>
 						<hr>

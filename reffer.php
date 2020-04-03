99f61707c74e30b4191062f2773e88a877389421
kwangaho <kwangaho@gmail.com>
2020-04-04 02:05:20 +0800

    Update

--- reffer.php
+++ reffer.php
@@ -38,41 +38,6 @@ if (isset($_GET['vid'])) {
 		header('location:'.urldecode($playurl));
 		exit;
 	}
-
-	if ($vid=='iptv805') {
-		$id=$_GET['id'];
-		$tid=$_GET['tid'];
-		if (!empty($_GET['p'])){$part='&p=' . $_GET['p'];}
-		$url="http://m.iptv805.com/iptv.php?act=play&tid=$tid&id=$id";
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_URL, $url);
-		curl_setopt($curl, CURLOPT_TIMEOUT, 5);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Linux; Android 8.0.0; Pixel 2 XL Build/OPD1.170816.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Mobile Safari/537.36');
-		$curlobj = curl_exec($curl);
-		curl_close($curl);
-		preg_match('/<option value="(.*?)"/i',$curlobj,$sn);
-		$linkurl=$sn[1] . $part;
-		$linkurl = preg_replace('#http://m.iptv.com/player.m3u8#', 'http://play.ggiptv.com:13164/play.m3u8', $linkurl);
-		if (strstr($linkurl, "ggiptv") != false){
-			$curl = curl_init();
-			curl_setopt($curl, CURLOPT_URL, $linkurl);
-			curl_setopt($curl, CURLOPT_NOBODY, 1);
-		    curl_setopt($curl, CURLOPT_AUTOREFERER, 1);
-	        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
-			curl_exec($curl);
-			$playurl = curl_getinfo($curl,CURLINFO_EFFECTIVE_URL);
-			if (isset($_GET['flv'])) {
-		    	$playurl = preg_replace('#/playlist.m3u8#', '.flv', $playurl);
-		    	$playurl = preg_replace('#.m3u8#', '.flv', $playurl);
-			}
-			header('location:'.$playurl);
-		} else {
-			header('location:'.$linkurl);
-		}
-		exit;
-	}
 	
 	if ($vid=='iptv234') {
 		$id=$_GET['id'];
@@ -104,7 +69,6 @@ if (isset($_GET['vid'])) {
 		header('location:'.$playurl);
 		exit;
 	}
-
 
 } else {
 	header('HTTP/1.1 403 Forbidden');
